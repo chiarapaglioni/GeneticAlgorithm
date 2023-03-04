247ebb45dea9153daf151fe46a8748d9232803c8
Chiara <paglionic@gmail.com>
2023-03-04 10:35:32 +0100

    Fitness Function Implementation

--- knapsack_problem/genetic_algo.py
+++ knapsack_problem/genetic_algo.py
@@ -38,7 +38,19 @@ def initialize_population(size, n):
 
 # Calculate the fitness of one individual based on the items (0 = excluded, 1 = included)
 def get_fitness(individual):
-    return
+    fitness = 0
+    for i in range(len(individual)):
+        if individual[i] == '1':
+            fitness = fitness + items[i].value
+    return fitness
+
+
+def get_population_fitness(popul):
+    fitness_vals = []
+    for i in range(len(popul)):
+        fitness = get_fitness(popul[i])
+        fitness_vals.append(fitness)
+    return fitness_vals
 
 
 def print_items(item_list):
@@ -55,11 +67,13 @@ def print_population(popul):
 if __name__ == '__main__':
     items = initialize_items(item_num)
     population = initialize_population(pop_size, item_num)
+    fitness_values = get_population_fitness(population)
 
     print('Items: ')
     print_items(items)
     print()
     print('Population: ')
     print_population(population)
-
-
+    print()
+    print('Fitness Values: ')
+    print_population(fitness_values)
